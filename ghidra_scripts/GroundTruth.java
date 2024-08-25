68948aa5bfefb4fb467c450fbb6d24bdf6987dd4
h1k0 <h1k0naka@outlook.com>
2024-08-25 21:38:54 +0800

    update: add GroundTruth Extractor

---
+++ ghidra_scripts/GroundTruth.java
@@ -0,0 +1,404 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import ghidra.app.decompiler.DecompInterface;
+import ghidra.app.decompiler.DecompileResults;
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.data.*;
+import ghidra.program.model.lang.Language;
+import ghidra.program.model.listing.Function;
+
+import blueprint.utils.*;
+import ghidra.program.model.pcode.HighFunction;
+import ghidra.program.model.pcode.HighSymbol;
+import ghidra.util.task.TaskMonitor;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.util.*;
+import java.io.File;
+
+public class GroundTruth extends GhidraScript {
+    private ObjectMapper objMapper;
+    private ObjectNode typeLibJsonRoot;
+    private ObjectNode varTypeJsonRoot;
+    private final Set<DataType> usedCompositeDT = new HashSet<>();
+    private final Map<Function, HighFunction> highFuncMap = new HashMap<>();
+
+    @Override
+    protected void run() throws Exception {
+
+        if(!Logging.init()) {
+            return;
+        }
+        if (!prepareAnalysis()) {
+            return;
+        }
+
+        objMapper = new ObjectMapper();
+        typeLibJsonRoot = objMapper.createObjectNode();
+        varTypeJsonRoot = objMapper.createObjectNode();
+        typeLibJsonRoot.set("struct", objMapper.createObjectNode());
+        typeLibJsonRoot.set("union", objMapper.createObjectNode());
+
+        // Function node and CallGraph statistics
+        Set<Function> meaningfulFunctions = FunctionHelper.getMeaningfulFunctions();
+        getVariableType(meaningfulFunctions);
+        getTypeLib();
+
+        // check if all composite data type appears in the variable type.
+        sanityCheck();
+    }
+
+
+    private void getTypeLib() {
+        for (var type: usedCompositeDT) {
+            if (type instanceof Structure) {
+                var structObj = typeLibJsonRoot.get("struct");
+                processStructure((Structure) type, (ObjectNode) structObj);
+            } else if (type instanceof Union) {
+                var unionObj = typeLibJsonRoot.get("union");
+                processUnion((Union) type, (ObjectNode) unionObj);
+            }
+        }
+
+        /* Save JSON to file: output/[binary_name]_typeLib.json */
+        saveJsonToFile(Global.outputDirectory + "/" + Global.currentProgram.getName() + "_typeLib.json", typeLibJsonRoot);
+    }
+
+
+    private void getVariableType(Set<Function> meaningfulFunctions) {
+        /* Decompile these functions */
+        DecompInterface ifc = DecompilerHelper.setUpDecompiler(null);
+        try {
+            if (!ifc.openProgram(Global.currentProgram)) {
+                Logging.error("GhidraScript", "Failed to use the decompiler");
+                return;
+            }
+
+            for (var func : meaningfulFunctions) {
+                DecompileResults decompileRes = ifc.decompileFunction(func, 30, TaskMonitor.DUMMY);
+                if (!decompileRes.decompileCompleted()) {
+                    Logging.error("CallGraph", "Decompile failed for function " + func.getName());
+                } else {
+                    highFuncMap.put(func, decompileRes.getHighFunction());
+                    Logging.info("CallGraph", "Decompile function " + func.getName());
+                }
+            }
+        } finally {
+            ifc.dispose();
+        }
+
+        /* Process each high function */
+        for (var func: highFuncMap.keySet()) {
+            var funcObj = objMapper.createObjectNode();
+            var highFunc = highFuncMap.get(func);
+            var localSymTable = highFunc.getLocalSymbolMap();
+            /* Process Parameters */
+            var paramObj = objMapper.createObjectNode();
+            for (var i = 0; i < localSymTable.getNumParams(); i++) {
+                var param = localSymTable.getParamSymbol(i);
+                var paramEntry = handleHighSymbol(param);
+                paramObj.set("Param_" + i, paramEntry);
+            }
+
+            /* Process Local Variables */
+            var localObj = objMapper.createObjectNode();
+            for (Iterator<HighSymbol> it = localSymTable.getSymbols(); it.hasNext(); ) {
+                var sym = it.next();
+                if (sym.isParameter()) { continue; }
+                if (sym.getHighVariable() == null) { continue; }
+                var varEntry = handleHighSymbol(sym);
+
+                if (sym.getStorage().isStackStorage()) {
+                    localObj.set(String.format("Stack[%d]", sym.getStorage().getStackOffset()), varEntry);
+                } else if (sym.getStorage().isRegisterStorage()) {
+                    localObj.set(String.format("Register[%s]", sym.getStorage().getRegister()), varEntry);
+                } else if (sym.getStorage().isUniqueStorage()) {
+                    localObj.set(String.format("Unique[%s]", sym.getStorage().getSerializationString()), varEntry);
+                }
+            }
+
+            funcObj.set("Parameters", paramObj);
+            funcObj.set("LocalVariables", localObj);
+            varTypeJsonRoot.set(func.getName(), funcObj);
+        }
+
+        /* Save JSON to file: output/[binary_name]_varType.json */
+        saveJsonToFile(Global.outputDirectory + "/" + Global.currentProgram.getName() + "_varType.json", varTypeJsonRoot);
+    }
+
+    private void sanityCheck() {
+        Set<String> missingTypes = new HashSet<>();
+
+        var functionNames = varTypeJsonRoot.fieldNames();
+        while (functionNames.hasNext()) {
+            var funcName = functionNames.next();
+            var funcObj = varTypeJsonRoot.get(funcName);
+
+            // Check Parameters
+            var paramObj = funcObj.get("Parameters");
+            if (paramObj != null) {
+                var paramNames = paramObj.fieldNames();
+                while (paramNames.hasNext()) {
+                    var paramName = paramNames.next();
+                    var param = paramObj.get(paramName);
+                    checkDataType((ObjectNode) param, missingTypes);
+                }
+            }
+
+            // Check Local Variables
+            var localObj = funcObj.get("LocalVariables");
+            if (localObj != null) {
+                var localNames = localObj.fieldNames();
+                while (localNames.hasNext()) {
+                    var localName = localNames.next();
+                    var localVar = localObj.get(localName);
+                    checkDataType((ObjectNode) localVar, missingTypes);
+                }
+            }
+        }
+
+        if (!missingTypes.isEmpty()) {
+            for (var missingType : missingTypes) {
+                Logging.error("sanityCheck", "Missing composite type in totalDT: " + missingType);
+            }
+        } else {
+            Logging.info("sanityCheck", "All composite types are accounted for.");
+        }
+    }
+
+    private void checkDataType(ObjectNode varNode, Set<String> missingTypes) {
+        String desc = varNode.get("desc").asText();
+        if (desc.contains("Struct") || desc.contains("Union")) {
+            String typeName = varNode.get("type").asText();
+
+            // Check if the type exists in totalDT
+            boolean typeExists = usedCompositeDT.stream().anyMatch(dt -> dt.getName().equals(typeName));
+            if (!typeExists) {
+                missingTypes.add(typeName);
+            }
+        }
+    }
+
+    private void saveJsonToFile(String fileName, ObjectNode jsonRoot) {
+        try {
+            objMapper.writerWithDefaultPrettyPrinter().writeValue(new File(fileName), jsonRoot);
+            Logging.info("GhidraScript", "Successfully wrote JSON to file: " + fileName);
+        } catch (IOException e) {
+            Logging.error("GhidraScript", "Error writing JSON to file: " + e.getMessage());
+        }
+    }
+
+
+    private ObjectNode handleHighSymbol(HighSymbol highSymbol) {
+        var result = objMapper.createObjectNode();
+        var dataType = highSymbol.getDataType();
+        var name = highSymbol.getName();
+
+        result.put("Name", name);
+        if (dataType instanceof TypeDef typeDef) {
+            dataType = typeDef.getBaseDataType();
+        }
+        if (dataType instanceof Pointer) {
+            var ptrLevel = 0;
+            while (dataType instanceof Pointer ptrDT) {
+                dataType = ptrDT.getDataType();
+                ptrLevel ++;
+            }
+            if (dataType instanceof TypeDef typeDef) {
+                dataType = typeDef.getBaseDataType();
+            }
+            if (dataType instanceof Structure) {
+                result.put("desc", "PointerToStruct");
+                usedCompositeDT.add(dataType);
+            } else if (dataType instanceof Union) {
+                result.put("desc", "PointerToUnion");
+                usedCompositeDT.add(dataType);
+            } else {
+                result.put("desc", "PointerToPrimitive");
+            }
+            result.put("type", dataType.getName());
+            result.put("ptrLevel", ptrLevel);
+        } else if (dataType instanceof Structure) {
+            result.put("desc", "Struct");
+            result.put("type", dataType.getName());
+            usedCompositeDT.add(dataType);
+        } else if (dataType instanceof Union) {
+            result.put("desc", "Union");
+            result.put("type", dataType.getName());
+            usedCompositeDT.add(dataType);
+        } else {
+            result.put("desc", "Primitive");
+            result.put("type", dataType.getName());
+        }
+
+        return result;
+    }
+
+
+
+    private void processStructure(Structure structure, ObjectNode structObj) {
+        Logging.info("GhidraScript","Structure: " + structure.getName());
+        var structNode = objMapper.createObjectNode();
+        var finalComponentMap = new TreeMap<Integer, DataType>();
+
+        /* Pre Process: find the base type of TypeDef */
+        for (var component: structure.getComponents()) {
+            var offset = component.getOffset();
+            var dataType = component.getDataType();
+
+            if (dataType instanceof TypeDef typeDef) {
+                Logging.info("GhidraScript", String.format("TypeDef detected at offset 0x%x", offset));
+                dataType = typeDef.getBaseDataType();
+                finalComponentMap.put(offset, dataType);
+            } else {
+                finalComponentMap.put(offset, dataType);
+            }
+        }
+
+        var layoutObj = objMapper.createObjectNode();
+        var PtrRelationObj = objMapper.createObjectNode();
+        var NestRelationObj = objMapper.createObjectNode();
+
+        /* Post Process */
+        for (var component: finalComponentMap.entrySet()) {
+            var fieldObj = objMapper.createObjectNode();
+            var offset = component.getKey();
+            var dataType = component.getValue();
+
+            /* Handle Layout */
+            if (dataType instanceof Structure) {
+                fieldObj.put("desc", "Nested");
+            } else if (dataType instanceof Union) {
+                fieldObj.put("desc", "Union");
+            } else if (dataType instanceof Array) {
+                fieldObj.put("desc", "Array");
+            } else if (dataType instanceof Pointer) {
+                fieldObj.put("desc", "Pointer");
+            } else {
+                fieldObj.put("desc", "Primitive");
+            }
+            fieldObj.put("size", dataType.getLength());
+            fieldObj.put("type", dataType.getName());
+            layoutObj.set("0x" + Integer.toHexString(offset), fieldObj);
+
+            /* Handle Relation */
+            if (dataType instanceof Pointer) {
+                int ptrLevel = 0;
+                var ptrEE = dataType;
+                while (ptrEE instanceof Pointer ptrDT) {
+                    ptrEE = ptrDT.getDataType();
+                    ptrLevel ++;
+                }
+
+                if (ptrEE instanceof TypeDef) {
+                    ptrEE = ((TypeDef) ptrEE).getBaseDataType();
+                }
+                if (ptrEE instanceof Structure || ptrEE instanceof Union) {
+                    var entry = objMapper.createObjectNode();
+                    entry.put("refType", ptrEE.getName());
+                    entry.put("ptrLevel", ptrLevel);
+                    PtrRelationObj.set("0x" + Integer.toHexString(offset), entry);
+                }
+            } else {
+                /* Handle Nest Relation */
+                if (dataType instanceof Structure) {
+                    var entry = objMapper.createObjectNode();
+                    entry.put("nestingType", dataType.getName());
+                    NestRelationObj.set("0x" + Integer.toHexString(offset), entry);
+                }
+            }
+        }
+
+        structNode.set("layout", layoutObj);
+        structNode.set("ptrRelation", PtrRelationObj);
+        structNode.set("nestRelation", NestRelationObj);
+        structObj.set(structure.getName(), structNode);
+    }
+
+    private void processUnion(Union union, ObjectNode unionObj) {
+        Logging.info("GhidraScript","Union: " + union.getName());
+        var unionNode = objMapper.createArrayNode();
+
+        for (var component: union.getComponents()) {
+            var memberNode = objMapper.createObjectNode();
+            var dataType = component.getDataType();
+
+            if (dataType instanceof TypeDef typeDef) {
+                Logging.info("GhidraScript", "TypeDef detected");
+                dataType = typeDef.getBaseDataType();
+            }
+
+            memberNode.put("Size", dataType.getLength());
+            memberNode.put("Type", dataType.getName());
+            memberNode.put("Name", component.getFieldName());
+            unionNode.add(memberNode);
+        }
+
+        unionObj.set(union.getName(), unionNode);
+    }
+
+
+    protected boolean prepareAnalysis() {
+        parseArgs();
+        prepareOutputDirectory();
+
+        Global.currentProgram = this.currentProgram;
+        Global.flatAPI = this;
+        Global.ghidraScript = this;
+
+        Language language = this.currentProgram.getLanguage();
+        if (language == null) {
+            Logging.error("GhidraScript","Language not found");
+            return false;
+        } else {
+            Logging.info("GhidraScript","Language: " + language.getLanguageID());
+            return true;
+        }
+    }
+
+    protected void parseArgs() {
+        String[] args = getScriptArgs();
+        for (String arg : args) {
+            Logging.info("GhidraScript", "Arg: " + arg);
+            // split the arguments string by "="
+            String[] argParts = arg.split("=");
+            if (argParts.length != 2) {
+                Logging.error("GhidraScript", "Invalid argument: " + arg);
+                System.exit(1);
+            }
+
+            String key = argParts[0];
+            String value = argParts[1];
+
+            if (key.equals("output")) {
+                Global.outputDirectory = value;
+            } else {
+                Logging.error("GhidraScript", "Invalid argument: " + arg);
+                System.exit(1);
+            }
+        }
+    }
+
+    protected void prepareOutputDirectory() {
+        if (Global.outputDirectory == null) {
+            Logging.error("GhidraScript","Output directory not specified");
+            System.exit(1);
+        }
+
+        File outputDir = new File(Global.outputDirectory);
+        // If the output directory does not exist, create it
+        if (!outputDir.exists()) {
+            if (!outputDir.mkdirs()) {
+                Logging.error("GhidraScript", "Failed to create output directory");
+                System.exit(1);
+            }
+        } else {
+            try {
+                FileUtils.cleanDirectory(outputDir);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
