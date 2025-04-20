2b1883f9fbe455191cf6b912ead76a0342fdd271
noobone123 <wangyanzhong@iie.ac.cn>
2025-04-20 23:39:46 +0800

    update LLM agent

--- ghidra_scripts/judge/main.py
+++ ghidra_scripts/judge/main.py
@@ -1,6 +1,45 @@
-def main():
-    print("Hello from judge!")
+import getpass
+import os
 
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    main()
+    load_dotenv()
+except ImportError:
+    pass
+
+os.environ["LANGSMITH_TRACING"] = "true"
+if "LANGSMITH_API_KEY" not in os.environ:
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if "LANGSMITH_PROJECT" not in os.environ:
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass.getpass(
+        prompt="Enter your OpenAI API key (required if using OpenAI): "
+    )
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+model = init_chat_model(
+    model = "gpt-4.1-mini",
+    temperature = 0.4,
+    base_url = os.environ.get("BASE_URL"),
+)
+
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
+
+model.invoke(messages)
